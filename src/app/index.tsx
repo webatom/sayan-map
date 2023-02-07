93f4687a925075448c9ff7e4108de127ff238974
Alexey Tomilov <suphuilo@gmail.com>
2023-02-08 00:50:52 +0800

    feat: 🎸 use open-topo-map

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -36,8 +36,8 @@ export const App: FC = () => {
       zoom={position.zoom}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Map style: &copy; <a href="https://www.opentopomap.org">OpenTopoMap</a> '
+        url="https://tile-{s}.opentopomap.cz/{z}/{x}/{y}.png"
       />
       <MapHooks />
       <Markers />
