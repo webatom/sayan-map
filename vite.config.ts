0a22d56be14b0e0b3bc97747bdba7f754c62b1b8
Alexey Tomilov <suphuilo@gmail.com>
2023-12-02 13:01:43 +0800

    feat: 🎸 add ya metrika

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ViteEjsPlugin } from 'vite-plugin-ejs';
 
 // https://vitejs.dev/config/
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [ViteEjsPlugin(), react()],
+});
